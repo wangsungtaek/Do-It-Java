a1ee79637310ac1b14f7c579af6cc770587013bf
Taek <68735491+wangsungtaek@users.noreply.github.com>
2020-12-15 00:59:59 -0400

    201214

--- src/stream/decorator/FileCopyTest.java
+++ src/stream/decorator/FileCopyTest.java
@@ -13,7 +13,7 @@ public class FileCopyTest {
 			millisecond = System.currentTimeMillis();
 			int i;
 			while((i=fis.read()) != -1) {
-				fos.write(i);
+			 	fos.write(i);
 			}
 			millisecond = System.currentTimeMillis() - millisecond;
 				
@@ -23,3 +23,4 @@ public class FileCopyTest {
 		System.out.println("파일 복사하는데 "+millisecond+"millisecond 소요되었습니다.");
 	}
 }
+ 
